cc32afac6d454f70a5ee08a57cde44a971057e4f
Murad-Hasan <mdmuradhasan151@gmail.com>
2025-08-28 17:18:48 +0600

    complete the call button functionalities

--- script.js
+++ script.js
@@ -3,12 +3,25 @@ function getElement(id){
     return value ;
 }
 
+
+
 getElement("card-container").addEventListener("click", function(e){
     const cardBtn = e.target;
-    
-    if(cardBtn.className.includes("call-btn")){
+    const everyCallCharge = 20;
+    const currentPoints = getElement("point").innerText;
+    const currentPointsNm = Number(currentPoints);
+
+    if(currentPointsNm < everyCallCharge) {
+        alert(
+          "You don't have enough coins. You need at least 20 coins to make a call."
+        );
+    }
+        
+    if(cardBtn.className.includes("call-btn") && currentPointsNm >= everyCallCharge){
         const serviceTitle = cardBtn.parentNode.parentNode.children[1].children[0].innerText;
         const serviceNumber = cardBtn.parentNode.parentNode.children[2].children[0].innerText;
+
+        getElement("point").innerText = currentPointsNm - everyCallCharge;
 
         const date = new Date().toLocaleTimeString();
         const parentDiv = getElement("parent");
@@ -16,14 +29,21 @@ getElement("card-container").addEventListener("click", function(e){
         childDiv.innerHTML = `
             <div class="bg-[#FAFAFA] flex justify-between items-center p-4 rounded-[8px] mb-2">
                 <div class="">
-                    <h2 class="font-semibold text-[18px]">Fire Service Number</h2>
-                    <p class="text-[#5C5C5C] text-[18px] mt-1">999</p>
+                    <h2 class="font-semibold text-[18px]">${serviceTitle}</h2>
+                    <p class="text-[#5C5C5C] text-[18px] mt-1">${serviceNumber}</p>
                 </div>
-                <div class="text-[18px]">12:22:00 pm</div>
+                <div class="text-[18px]">${date}</div>
             </div>
         `;
         parentDiv.appendChild(childDiv);
+    
         
-        alert('card added')
+        
+        const alerts =   "Calling" + " " + serviceTitle + " " + serviceNumber + "...";
+        alert(alerts)
     }
+    
 });
+
+
+
